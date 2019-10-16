6e86c3a8db88e665a7f95cee8d3b55490edd36e1
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-16 18:38:47 -0500

    updated scrape function

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,47 +1,44 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-module.exports = {
-    scrape: cb => {
-        //make axios to nytimes homepage
-        axios.get("http://www.nytimes.com/")
-            .then(res => {
-                //axios being loaded by cheerio
-                const $ = cheerio.load(res.data);
-                const headlines = [];
+const scrape = cb => {
+    //make axios to nytimes homepage
+    axios.get("http://www.nytimes.com/")
+        .then(res => {
+            //axios being loaded by cheerio
+            const $ = cheerio.load(res.data);
+            const headlines = [];
 
-                $('article div.assetwrapper div a').each(
-                    (i, element) => {
-                        //array to store all valid elements
+            $('div.assetWrapper a').each(
+                (i, element) => {
+                    //array to store all valid elements
+
+                    let title = $(this)
+                        .find('h2')
+                        .text()
+                        .trim();
+
+                    let link = $(this)
+                        .attr('href');
+
+                    let summary = $(this)
+                        .find('p')
+                        .text()
+                        .trim();
+
+                    if (title && link && summary) {
+                        //create an object headline if all items exist;
+                        title = title.replace(/(r\n|\n|\r|\t|\s+)/gm, '').trim();
+                        summary = summary.replace(/(r\n|\n|\r|\t|\s+)/gm, '').trim();
                         
-                        let title = $(this)
-                            .children('div')
-                            .children('h2')
-                            .text()
-                            .trim();
+                        const headline = {title,link,summary};
 
-                        let link = $(this)
-                            .attr('href');
-                        
-                        let summary = $(this)
-                            .children('p')
-                            .text()
-                            .trim();
+                        //push to array if all items exist
+                        headlines.push(headline)
+                    }
+                });
+            cb(articles);
+        });
+}
 
-                        if (title && link && summary){
-                            //create an object headline if all items exist;
-                            const headline = {};
-                            const titleNeat = title.replace(/(r\n|\n|\r|\t|\s+)/gm,'').trim();
-                            const summaryNeat = summary.replace(/(r\n|\n|\r|\t|\s+)/gm,'').trim();
-                            headline.title = titleNeat;
-                            headline.link = link;
-                            headline.summary = summaryNeat
-                            
-                            //push to array if all items exist
-                            headlines.push(headline)
-                        }
-                    });
-                    cb(articles);
-            });
-    }
-};
+module.exports = scrape;
