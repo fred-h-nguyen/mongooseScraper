6ae1d3bd2ef71cd984184e5b9114ebb6aa04a9b0
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-20 21:32:52 -0500

    added mongo database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Requiring npm packages
-var express = require("express");
-var exphbs = require("express-handlebars")
-
+const express = require("express");
+const exphbs = require("express-handlebars")
+const mongoose = require("mongoose");
 //creating app instance
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +17,14 @@ app.engine("handlebars",exphbs({
     })
 );
 app.set("view engine", "handlebars");
+
+//mongoose database
+const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+
+//connecting to the database
+mongoose.connect(db,{useNewUrlParser:true})
+//routes
+require('./routes/routes')(app);
 
 //Listening on the port
 app.listen(PORT, function () {
