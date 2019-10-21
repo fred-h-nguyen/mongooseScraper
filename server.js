b5fc26c6a9bd564b3aeb1ec7806c64fa14775357
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-20 21:43:55 -0500

    finished server setup and connection to databse

--- server.js
+++ server.js
@@ -2,34 +2,36 @@
 const express = require("express");
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose");
+const logger = require('morgan')
 //creating app instance
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Handlebars
-app.engine("handlebars",exphbs({
+app.engine('handlebars',exphbs({
         defaultLayout: "main"
     })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 //mongoose database
-const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+const db = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadline'
 
 //connecting to the database
-mongoose.connect(db,{useNewUrlParser:true})
+mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
 //routes
 require('./routes/routes')(app);
 
 //Listening on the port
 app.listen(PORT, function () {
     console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
         PORT,
         PORT
     );
