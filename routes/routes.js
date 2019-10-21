684ad6f9374ab81803610cecfe1b101226bba8c0
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-20 21:32:23 -0500

    rewrote routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,15 @@
-const scrape = require('../scripts/scrape');
-
 const controller = require('../controllers');
 
-module.exports = router => {
-    router.get('/', (req, res) => {
+module.exports = app => {
+    app.get('/', (req, res) => {
         res.render('home');
     });
 
-    router.get('/saved', (req, res) => {
+    app.get('/saved', (req, res) => {
         res.render('saved');
     });
 
-    router.get('/api/scrape', (req, res) => {
+    app.get('/api/scrape', (req, res) => {
         controller.headlines.scrape((err, docs) => {
             if (!docs || docs.insertedCount === 0) {
                 res.json({ message: 'No new articles today. Check back tomorrow!' });
@@ -23,7 +21,7 @@ module.exports = router => {
         });
     });
 
-    router.get('/api/headlines', (req, res) => {
+    app.get('/api/headlines', (req, res) => {
         const query = {}
         if (req.query.saved) {
             query = req.query;
@@ -34,7 +32,7 @@ module.exports = router => {
         });
     });
 
-    router.delete('/api/headlines/:id', (req, res) => {
+    app.delete('/api/headlines/:id', (req, res) => {
         const query = {};
         query._id = req.params.id;
         controller.headlines.delete(query,result=>
@@ -43,14 +41,14 @@ module.exports = router => {
         });
     });
 
-    router.patch('/api/headlines',(req,res)=>
+    app.patch('/api/headlines',(req,res)=>
     {
         controller.headlines.update(req.body,result=>{
             res.json(result);
         });
     });
 
-    router.get('/api/notes/:headline_id',(req,res)=>{
+    app.get('/api/notes/:headline_id',(req,res)=>{
         const query = {};
         query._id = req.params.headline_id;
         controller.notes.get(query,result=>{
@@ -58,7 +56,7 @@ module.exports = router => {
         });
     });
 
-    router.post('/api/notes/:headline_id',(req,res)=>{
+    app.post('/api/notes/:headline_id',(req,res)=>{
         const query = {};
         query._id = req.params.headline_id
         controller.notes.save(req.body,query,result=>{
@@ -66,7 +64,7 @@ module.exports = router => {
         });
     });
 
-    router.delete('/api/notes/:id',(req,res)=>{
+    app.delete('/api/notes/:id',(req,res)=>{
         const query= {};
         query._id = req.params.id;
         controller.notes.delete(query,result=>{
